121e4002e2a26e563b8fe9e2ac5c36bbd315fda1
Sercan Çalış <34138084+sercancalis@users.noreply.github.com>
2020-01-05 14:50:38 +0300

    Add files via upload

---
+++ MusicStore.UI.MVC/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using MusicStore.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.UI.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _CartButton()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AddToCart(AlbumViewModel album)
+        {
+            MyCart cart = Session["cart"] as MyCart;
+            CartItemViewModel cartItem = new CartItemViewModel();
+
+            cartItem.ID = album.AlbumID;
+            cartItem.Name = album.Title;
+            cartItem.Price = album.Discounted ? album.Price * 0.9m : album.Price;
+            cartItem.Amount = 1;
+            cart.AddCart(cartItem);
+            Session["cart"] = cart;
+            return PartialView("_CartButton");
+        }
+
+        public ActionResult _CartList()
+        {
+            return PartialView();
+        }
+        public ActionResult UpdateCart(short amount, int id)
+        {
+            MyCart guncellenenSepet = Session["cart"] as MyCart;
+            guncellenenSepet.Update(id, amount);
+            Session["cart"] = guncellenenSepet;
+            return RedirectToAction("_CartList", "Cart");
+        }
+        public ActionResult DeleteItemCart(int id)
+        {
+            MyCart silinecekCart = Session["cart"] as MyCart;
+            silinecekCart.Delete(id);
+            Session["cart"] = silinecekCart;
+            return RedirectToAction("_CartList", "Cart");
+        }
+
+
+    }
+}
---
+++ MusicStore.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using MusicStore.BLL.Abstract;
+using MusicStore.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        IGenreService _genreService;
+        public HomeController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAlbums(List<AlbumViewModel> albums)
+        {
+            if (albums == null)
+            {
+                ViewBag.Error = "Ürün bulunamadı";
+                return PartialView("_AlbumPartial");
+            }
+            return View("_AlbumPartial",albums);
+        }
+
+        public ActionResult _GenrePartial()
+        {
+            return PartialView(_genreService.GetAll());
+        }
+
+
+    }
+}
